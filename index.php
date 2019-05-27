525d1e70e85b10d6704bdd88662902e6c43b2cf9
ebergez <emilebergez@gmail.com>
2019-05-27 23:29:00 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Resal social</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<section class="leftNav">
+	<img src="" alt="">
+</section>
+  <header>
+		<input type="search" name="research" placeholder="Recherche" class="searchBar">
+	</header>
+	<section class="rightNav">
+	<img src="" alt="">
+</section>
+<section class="test"></section>
+	
+</body>
+</html>
